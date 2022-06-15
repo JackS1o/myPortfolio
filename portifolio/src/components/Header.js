0a27d7e74f0e96aadf1a701e52108adcfdd847d2
Jackson <jviana116@gmail.com>
2022-06-15 12:10:18 -0300

    adc css

---
+++ portifolio/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav1">
+        Início
+      </nav>
+      <nav className="nav2">
+        Sobre Mim
+      </nav>
+      <nav className="nav3">
+        Projetos
+      </nav>
+      <nav className="nav4">
+        Contato
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- portifolio/src/components/InicialPage.js
+++ portifolio/src/components/InicialPage.js
@@ -1,11 +1,27 @@
 import React from "react";
+import Header from "./Header";
 
 function InicialPage() {
-    return (
-        <div>
-            ola
+  return (
+    <section>
+      <Header />
+      <div className="mainDivInicialPage">
+        <div className="divIntroducao">
+          <h4 className="p-ola">
+            Olá, meu nome é
+          </h4>
+          <h1>
+            JACKSON VIANA
+            <br />
+            FRONT-END DEVELOPER
+          </h1>
         </div>
-    );
+        <div className="divImg">
+          img
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default InicialPage;
